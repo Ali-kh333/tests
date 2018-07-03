92cb07bf6304c307a223bb6e1b3dbcb63649c0e9
Ali Khalili <ali.khalili5544@gmail.com>
2018-07-03 15:09:50 +0430

    te

--- src/BladeTestServiceProvider.php
+++ src/BladeTestServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Khalili\Blade;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class BladeTestServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,10 @@ class BladeTestServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__ . '/views', 'btest');
+
+        Blade::directive('hi', function () {
+            return "<?php echo hey; ?>";
+        });
     }
 
     /**
